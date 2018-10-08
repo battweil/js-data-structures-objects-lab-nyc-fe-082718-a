ba965da0b302ca7d9d4bf4e766dcdac5102a0052
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-08 01:30:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const driver = {
 function updateDriverWithKeyAndValue(driver, key, value){
   driver.key = value;
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  
+}
+
