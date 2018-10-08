7620651a920793272b062463d93455288533ca5c
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-08 01:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,23 @@
 // function destructivelyDeleteFromDriverByKey(driver, key){
   
 // }
+
+const driver =
+{ foo: "bar"};
+ function updateDriverWithKeyAndValue(driver, key, value){
+  const newDriver = { ...driver };
+  newDriver[key] = value;
+  return newDriver;
+}
+ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+   driver[key] = value;
+  return driver;
+}
+ function deleteFromDriverByKey(driver, key){
+  const newDriver = { ...driver };
+  delete newDriver[key];
+  return newDriver;
+}
+ function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key];
+  return driver;
