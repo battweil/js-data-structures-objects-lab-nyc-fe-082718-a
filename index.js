608e51b4febde0b1ff0335f2e76f9609115170dd
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-08 01:47:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,18 @@ const driver =
   newDriver[key] = value;
   return newDriver;
 }
+
  function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
    driver[key] = value;
   return driver;
 }
+
  function deleteFromDriverByKey(driver, key){
   const newDriver = { ...driver };
   delete newDriver[key];
   return newDriver;
 }
+
  function destructivelyDeleteFromDriverByKey(driver, key){
   delete driver[key];
   return driver;
